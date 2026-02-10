a6d40cf02c3ab660630512e7f5b6e3e99baf2f82
zirunwang677-alt <zirunwang677@gmail.com>
2026-02-10 23:39:04 +0800

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'score-board-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+// 安装服务：缓存文件
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// 激活服务：清理旧缓存
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+// 拦截请求：优先使用离线缓存
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
